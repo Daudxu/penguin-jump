9e3a7005f5d675c1f7b13f7b668a56f0c38784c9
xudan <xudan@apowo.com>
2023-03-15 16:27:54 +0800

    fix update start and bgm

---
+++ src/App.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="cl-start-shade" v-if="isStart === 0">
+     <div class="cl-start-main"> 
+          <div class="cl-title"> jump jump jump </div>
+          <div class="cl-button" @click="handleClickStart">start</div>
+     </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+
+import JumpGame from './object/JumpGame';
+
+onMounted (()=>{
+  new JumpGame().start();
+})
+
+const isStart = ref(0)
+
+const handleClickStart = () => {
+  isStart.value = 1
+  audioBgm()
+}
+
+const audioBgm = () => {
+	var bgm = new Audio('./audio/bgm.mp3');
+	bgm.volume = .07
+	bgm.play();
+  bgm.loop = true;
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+body{
+	padding: 0;
+	margin: 0;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	user-select: none;
+  overflow: hidden;
+}
+
+.cl-start-shade {
+  position: fixed;
+  top: 0;
+  width:100vw;
+  height:100vh;
+  z-index: 999;
+  background: rgba(255, 255, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.cl-start-shade .cl-start-main{
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+}
+
+.cl-start-shade .cl-title {
+  font-size: 26px;
+  font-weight: 600;
+  margin-bottom: 30px;
+}
+
+.cl-start-shade .cl-button {
+  font-size: 26px;
+  font-weight: 600;
+  width: 150px;
+  height: 60px;
+  line-height: 60px;
+  color: #ffffff;
+  background: #232323;
+  border-radius: 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  /* margin: 90px; */
+}
+
+.mask{
+	display: none;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	background: rgba(0,0,0,0.4);
+}
+.content{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 500px;
+	height: 500px;
+	border-radius: 20px;
+	background: rgba(0,0,0,0.4);
+	border: 5px solid rgba(255,255,255,0.05);
+
+}
+.score-container{
+	color: #ffffff;
+	/* color: #232323; */
+	text-align: center;
+}
+.title{
+  font-size: 20px;
+  font-weight: bold;
+  /* color: rgba(255,255,255,0.6); */
+}
+.score{
+	font-size: 100px;
+	font-weight: bold;
+	margin-top: 20px;
+}
+button.restart{
+	width: 200px;
+	height: 40px;
+	border-radius: 20px;
+	background: white;
+	border: none;
+  font-weight: bold;
+  font-size: 20px;
+  cursor: pointer;
+}
+button.restart:hover{
+	color:#232323;
+}
+.info{
+  margin: 20px 0;
+  position: absolute;
+  text-align: center;
+  width:100%;
+  color: rgba(255,255,255,0.2);
+}
+.info a{
+  /* display: block; */
+  font-size: 16px;
+  line-height: 28px;
+  color: rgba(255,255,255,0.2);
+	/* color: #232323; */
+  text-decoration: none;
+}
+a.title{
+  font-size: 20px;
+  font-weight: bold;
+}
+.score-gaming{
+	margin-top: 10px;
+	color: rgba(255,255,255,1);
+	font-size: 16px;
+}
+</style>
---
+++ src/config/settings.js
@@ -0,0 +1,31 @@
+const VERSION = import.meta.env.VITE_VERSION
+const IS_MOBILE = true
+const IS_HELPER = true
+const BACKGROUND = '0x282828'
+const GROUND = -1
+const FALLING_SPEED = 0.2
+const CUBE_COLOR = '0xbebebe'
+const CUBE_WIDTH = 3
+const CUBE_HEIGHT = 1
+const CUBE_DEEP = 3
+const JUMPER_COLOR = '0x333333'
+const JUMPER_WIDTH = 1
+const JUMPER_HEIGHT = 1
+const JUMPER_DEEP = 1
+
+export  {
+    VERSION,
+    IS_MOBILE,
+    IS_HELPER,
+    BACKGROUND,
+    GROUND,
+    FALLING_SPEED,
+    CUBE_COLOR,
+    CUBE_WIDTH,
+    CUBE_HEIGHT,
+    CUBE_DEEP,
+    JUMPER_COLOR,
+    JUMPER_WIDTH,
+    JUMPER_HEIGHT,
+    JUMPER_DEEP
+}
