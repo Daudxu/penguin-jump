2d6c32666f54375664f4533c8de0e0cad7b1fb09
xudan <875126243@qq.com>
2023-03-23 15:09:52 +0800

    fix update game

--- src/object/CylinderBox.js
+++ src/object/CylinderBox.js
@@ -1,5 +1,6 @@
 import Box from './Box';
-import {CylinderGeometry, Mesh, MeshLambertMaterial} from "three";
+// import {CylinderGeometry, Mesh, MeshLambertMaterial} from "three";
+import * as THREE from "three";
 
 export default class CylinderBox extends Box {
   constructor(prev) {
@@ -8,10 +9,17 @@ export default class CylinderBox extends Box {
 
   // 生成盒子
   initBox() {
-    const geometry = new CylinderGeometry(this.size/2, this.size/2, this.height, 50);
-    const material = new MeshLambertMaterial({color: this.color,});
 
+    const geometry = new THREE.CylinderGeometry(this.size/2, this.size/2, this.height, 50);
+    // const material = new THREE.MeshLambertMaterial({color: this.color,});
+    const textureLoader = new THREE.TextureLoader();
+    const material1 = new THREE.MeshLambertMaterial({
+      map: textureLoader.load('./textures/box.jpg'),
+    })
+    const material2 = new THREE.MeshLambertMaterial({
+      map: textureLoader.load('./textures/ice.png'),
+    })
     geometry.translate(0, this.height/2, 0);
-    this.mesh = new Mesh(geometry, material);
+    this.mesh = new THREE.Mesh(geometry, [material1, material2]);
   }
 }
